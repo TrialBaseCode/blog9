8018575eaa3944955e9ed48039156744ae78d088
TrialBaseCode <compwork321@gmail.com>
2023-05-04 12:57:25 +0530

    laravel project

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\dummyAPI;
 use App\Http\Controllers\DeviceController;
+use App\Http\Controllers\MemberApiController;
+use App\Http\Controllers\SacUserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +22,22 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//  use sctrum
 
-Route::post("Listadd" , [DeviceController::class,'listdataadd']);
-Route::put("Listupdate" , [DeviceController::class,'listdataupdate']);
-Route::delete("Listdelete/{id}" , [DeviceController::class,'listdatadelete']);
-Route::get("Listsearch/{name}" , [DeviceController::class,'listdatasearch']);
-Route::post("Listvalidate" , [DeviceController::class,'listdatavalidate']);
+Route::group(['middleware' => 'auth:sanctum'], function(){
+    //All secure URL's
+    Route::apiResource("AllMember", MemberApiController::class);
+    Route::post("Listadd" , [DeviceController::class,'listdataadd']);
+    Route::put("Listupdate" , [DeviceController::class,'listdataupdate']);
+    Route::delete("Listdelete/{id}" , [DeviceController::class,'listdatadelete']);
+    Route::get("Listsearch/{name}" , [DeviceController::class,'listdatasearch']);
+    Route::post("Listvalidate" , [DeviceController::class,'listdatavalidate']);
+});
+
+
+
+
+
+// user sactum
+
+Route::post("saclogin",[SacUserController::class,'index']);
